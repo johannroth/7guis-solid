59de992b285df8ca6ead927a8ffb6e4c0266d642
Johann Roth <johann.roth@esveo.com>
2022-03-21 14:06:17 +0100

    adds solid/parcel/typescript stack

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 7guis-solid
+
+using starter template from https://codesandbox.io/s/solid-tsx-starter-x2u2h
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { Component } from "solid-js";
+import { render } from "solid-js/web";
+
+const App: Component = () => {
+  return <h1>Hello, world!</h1>;
+};
+
+render(() => <App />, document.getElementById("app"));
